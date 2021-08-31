0e3763bb8422c861f74b4eac916abb9496c576e4
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-08-30 21:49:30 -0700

    Modding the packagejson

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
 } else {
  sequelize = process.env.JAWSDB_URL
      ? new Sequelize(process.env.JAWSDB_URL)
-     :new Sequelize(
+     : new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
         process.env.DB_PASSWORD,
