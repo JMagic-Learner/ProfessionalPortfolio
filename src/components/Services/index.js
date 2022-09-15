4d42ae680994f06764a050697d2b50e018762153
JasonDesktop <MaJason93@gmail.com>
2022-09-15 12:45:08 -0700

    Testing and fixing overlays

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,61 +1,61 @@
 import React from 'react'
-
+import ContactImage from '../../img/JasonMaBanner.jpg'
 export default function Services() {
 
-    let HTMLarray = [
-      {
-        icon:"ion-monitor",
-        title:"REACT Applications",
-        description:"Using modern, modular frameworks to build solutions."
-      },
-      {
-        icon:"ion-android-checkbox",
-        title:"Test Driven Development",
-        description:" Integral in planning out behavior and expected end-user interaction."
-      },
-      {
-        icon:"ion-android-phone-portrait",
-        title:"Responsive Design",
-        description:"Utlize MUI-REACT, SASS, Bootstrap to code responsive layouts."
-      },
-    ]
-    
-    return <section id="service" className="services-mf route">
+  let HTMLarray = [
+    {
+      icon: "ion-monitor",
+      title: "REACT Applications",
+      description: "Using modern, modular front-end frameworks to build solutions."
+    },
+    {
+      icon: "ion-android-checkbox",
+      title: "Test Driven Development",
+      description: " Integral in planning out behavior and expected end-user interaction."
+    },
+    {
+      icon: "ion-android-phone-portrait",
+      title: "Responsive Design",
+      description: "Utlize MUI-REACT, SASS, Bootstrap to code responsive layouts."
+    },
+  ]
+
+  return <section id="services" className="services-mf route paralax-mf bg-image" style={{backgroundImage: `url(${ContactImage})`}}>
     <div className="container">
       <div className="row">
-       
+
         <div className="col-sm-12">
           <div className="title-box text-center">
             <h3 className="title-a">
               Services
             </h3>
             <p className="subtitle-a">
-             Bringing efficient solutions to clients.
+              Bringing efficient solutions to clients.
             </p>
             <div className="line-mf"></div>
           </div>
         </div>
       </div>
-        <div className="row" >
-        {HTMLarray.map((item)=>{
-          return(
+      <div className="row" >
+        {HTMLarray.map((item) => {
+          return (
             <div className="col-md-4">
-          <div className="service-box">
-            <div className="service-ico">
-              <span className="ico-circle"><i className={item.icon}></i></span>
+              <div className="service-box">
+                <div className="service-ico">
+                  <span className="ico-circle"><i className={item.icon}></i></span>
+                </div>
+                <div className="service-content">
+                  <h2 className="s-title">{item.title}</h2>
+                  <p className="s-description text-center">
+                    {item.description}
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="service-content">
-              <h2 className="s-title">{item.title}</h2>
-              <p className="s-description text-center">
-                {item.description}
-              </p>
-            </div>
-          </div>
-        </div>
           )
         })}
 
-        </div>
+      </div>
     </div>
   </section>
 }
